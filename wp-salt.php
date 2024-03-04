6068a265574f0d931033e89b4943ed93ace6068a
Emotsy <web@emotsy.com>
2024-03-04 14:37:40 +0000

    backup

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'HBtUTDhLXTUEdGroc2M6hnv0YPFym2YCovxWGb8qN1IretWvtxwdqUac2QxA8tw6');
+define('SECURE_AUTH_KEY',  'C2GRfI77MatIoqK1B0r7XV52YeguNNwT8iB19RPypVpyanfgfpxcwvLAgJjrEtFS');
+define('LOGGED_IN_KEY',    'FuwFjR3vMsDd8mGFWw2XGARNiWD01ipmyW1KSp6Ss8b03bF0Po87hSbbr2cDsRFx');
+define('NONCE_KEY',        'iyKYRyShgxRUHFLzLmsLSwiV5Bn8mtRejSwXp5WML8PcJUzQN32Xj6hFqfwBQthK');
+define('AUTH_SALT',        'p2vx3UQvvY4bXnW9f6oWjY3AygENozQpayaICJGUIeUx1RwGiRH02weQI2HtFR9B');
+define('SECURE_AUTH_SALT', 'a3BXfYvmXYNondmNUGBVPpJCWsq8NpT0sUU0XTFmWfLwd27d2ymFsHvxFd2XvEHS');
+define('LOGGED_IN_SALT',   'e0UgG4UpK0CLYXXKUDw4Wda3Cup5TmwMRu9eDDGQGDR7FwM9RmauXPbum1BH6KeH');
+define('NONCE_SALT',       'L5wpz3hi6fP5dvvvVch75Yfb3xsLAvRLN6NaipiRSdJ7attPuuB8X9avNBgPWLRG');
